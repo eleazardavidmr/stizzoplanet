2bcf6e29dc996ad2c5f87d94d94e86b78cb366e0
Eleazar (Linux) <eleazardavidmr@gmail.com>
2025-01-06 16:34:08 -0500

    cambio en la fuente del filternav

--- src/Components/FilterNav/index.jsx
+++ src/Components/FilterNav/index.jsx
@@ -1,7 +1,6 @@
 import { useContext } from "react";
 import { ProductContext } from "../Context";
 import { NavLink } from "react-router-dom";
-import { motion } from "framer-motion";
 export default function FilterNav() {
   const context = useContext(ProductContext);
   return (
@@ -11,7 +10,7 @@ export default function FilterNav() {
           to="/dama"
           className={({ isActive }) => {
             return isActive
-              ? "text-sm px-4 py-1.5 md:px-5 md:py-2.5 text-center text-[12px] md:text-sm me-2 mb-2 border-2 bg-primary/60 border-primary/60 hover:bg-primary text-white rounded-full"
+              ? "text-sm px-4 py-1.5 font-bold transition-all md:px-5 md:py-2.5 text-center text-[12px] md:text-sm me-2 mb-2 border-2 bg-primary/60 border-primary/60 hover:bg-primary text-white rounded-full"
               : "text-sm px-4 py-1.5 md:px-5 md:py-2.5 text-center text-[12px] md:text-sm me-2 mb-2 border-2 border-primary/60 hover:bg-primary text-white rounded-full";
           }}
           onClick={() => context.setSelectedCategory("dama")}
@@ -22,7 +21,7 @@ export default function FilterNav() {
           to="/"
           className={({ isActive }) => {
             return isActive
-              ? "text-sm px-4 py-1.5 md:px-5 md:py-2.5 text-center me-2 mb-2 text-[12px] md:text-sm border-2 bg-primary/60 border-primary/60 hover:bg-primary text-white rounded-full"
+              ? "text-sm px-4 py-1.5 font-bold transition-all md:px-5 md:py-2.5 text-center me-2 mb-2 text-[12px] md:text-sm border-2 bg-primary/60 border-primary/60 hover:bg-primary text-white rounded-full"
               : "text-sm px-4 py-1.5 md:px-5 md:py-2.5 text-center me-2 mb-2 text-[12px] md:text-sm border-2 border-primary/60 hover:bg-primary text-white rounded-full";
           }}
           onClick={() => context.setSelectedCategory(null)}
@@ -33,7 +32,7 @@ export default function FilterNav() {
           to="/caballero"
           className={({ isActive }) => {
             return isActive
-              ? "text-sm px-4 py-1.5  md:px-5 md:py-2.5 text-center me-2 mb-2 text-[12px] md:text-sm border-2 bg-primary/60 border-primary/60 hover:bg-primary text-white rounded-full"
+              ? "text-sm px-4 py-1.5 font-bold transition-all md:px-5 md:py-2.5 text-center me-2 mb-2 text-[12px] md:text-sm border-2 bg-primary/60 border-primary/60 hover:bg-primary text-white rounded-full"
               : "text-sm px-4 py-1.5 md:px-5 md:py-2.5 text-center me-2 mb-2  text-[12px] md:text-sm border-2 border-primary/60 hover:bg-primary text-white rounded-full";
           }}
           onClick={() => context.setSelectedCategory("caballero")}
